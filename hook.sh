cafb965e923ec5e32e6a0b1b09623531447eb48b
Jinhyung Park <jeenhyung@hanmail.net>
2017-09-21 16:51:22 +0900

    modify hook.sh

--- hook.sh
+++ hook.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /Users/jeenhyung-mac/develop/github_storage/myrepo/node-sample-release
-git reset --hard HEAD^
-git pull
-pm2 restart node-sample
+sudo cd /Users/jeenhyung-mac/develop/github_storage/myrepo/node-sample-release
+sudo git reset --hard HEAD^
+sudo git pull
+sudo pm2 restart node-sample
 #pm2 stop node-sample # replace futsal to your pm2 project name 
 #pm2 start node-sample # replace futsal to your pm2 project name 
