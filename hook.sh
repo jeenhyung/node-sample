5d467b523588f902f5189d58a95b2173920c8bee
Jinhyung Park <jeenhyung@hanmail.net>
2017-09-21 20:49:36 +0900

    modify hoook.sh

--- hook.sh
+++ hook.sh
@@ -3,6 +3,6 @@
 cd /Users/jeenhyung-mac/develop/github_storage/myrepo/node-sample-release
 git reset --hard HEAD^
 git pull
-/usr/local/bin/npm install
-/usr/local/bin/pm2 restart node-sample
+npm install
+pm2 restart node-sample
 
