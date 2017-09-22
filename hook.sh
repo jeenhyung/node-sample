8678d498b7c9ee706879ea4b5847d6dbc1f22761
Jinhyung Park <jeenhyung@hanmail.net>
2017-09-22 11:18:53 +0900

    modify hook.sh

--- hook.sh
+++ hook.sh
@@ -4,5 +4,5 @@ cd /Users/jeenhyung-mac/develop/github_storage/myrepo/node-sample-release
 git reset --hard HEAD^
 git pull
 npm install
-pm2 restart node-sample
-
+pm2 stop node-sample
+pm2 start src/app.js --name "node-sample"
