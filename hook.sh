8e8be4e5f01edc5ebd27c3b0ba2953b3903660de
Jinhyung Park <jeenhyung@hanmail.net>
2017-09-22 11:10:42 +0900

    hook.sh

--- hook.sh
+++ hook.sh
@@ -3,7 +3,6 @@
 cd /Users/jeenhyung-mac/develop/github_storage/myrepo/node-sample-release
 git reset --hard HEAD^
 git pull
-node src/app.js
-#npm install
-#pm2 restart node-sample
+npm install
+pm2 restart node-sample
 
