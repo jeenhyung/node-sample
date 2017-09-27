255bf1f25952c92706aee8e03eb792bc7c5eacec
Jinhyung Park <jeenhyung@hanmail.net>
2017-09-27 16:50:22 +0900

    modify install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,13 @@ yum -y update
 yum -y install default-jre ImageMagick
 
 # add nodejs to yum
-curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
-yum -y install nodejs #default-jre ImageMagick
+#curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+#yum -y install nodejs #default-jre ImageMagick
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash    # for amazon linux
+. ~/.nvm/nvm.sh
+nvm install 6.9.1
+node -e "console.log('Running Node.js ' + process.version)"
+
 
 # install pm2 module globaly
 npm install -g pm2
