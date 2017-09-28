58924b487cd97b104ffbf848dc9137ccd7ac9418
Jinhyung Park <jeenhyung@hanmail.net>
2017-09-28 17:13:19 +0900

    modify install.sh & post_install.sh

--- install.sh
+++ install.sh
@@ -7,18 +7,18 @@ yum -y update
 # install general libraries like Java or ImageMagick
 yum -y install default-jre ImageMagick
 
-# add nodejs to yum
-#curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-#yum -y install nodejs #default-jre ImageMagick
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash    # for amazon linux
-. ~/.nvm/nvm.sh
-nvm install 6.9.1
-node -e "console.log('Running Node.js ' + process.version)"
+# # add nodejs to yum
+# #curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+# #yum -y install nodejs #default-jre ImageMagick
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash    # for amazon linux
+# . ~/.nvm/nvm.sh
+# nvm install 6.9.1
+# node -e "console.log('Running Node.js ' + process.version)"
 
 
-# install pm2 module globaly
-npm install -g pm2
-pm2 update
+# # install pm2 module globaly
+# npm install -g pm2
+# pm2 update
 
-# cd ~/node
-# /home/ec2-user/.nvm/versions/node/v6.9.1/lib/node_modules/npm/bin/npm-cli.js install
+# # cd ~/node
+# # /home/ec2-user/.nvm/versions/node/v6.9.1/lib/node_modules/npm/bin/npm-cli.js install
--- post_install.sh
+++ post_install.sh
@@ -2,6 +2,16 @@
 #!/usr/bin/env bash
 set -e
 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash    # for amazon linux
+. ~/.nvm/nvm.sh
+nvm install 6.9.1
+node -e "console.log('Running Node.js ' + process.version)"
+
+
+# install pm2 module globaly
+npm install -g pm2
+pm2 update
+
 cd ~/node
 npm install
 # /home/ec2-user/.nvm/versions/node/v6.9.1/lib/node_modules/npm/bin/npm-cli.js install
