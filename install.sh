11535c03354fe401d726028ee7cb6b0af61a50a1
Jinhyung Park <jeenhyung@hanmail.net>
2017-10-11 14:20:54 +0900

    modify all

--- install.sh
+++ install.sh
@@ -7,24 +7,24 @@ yum -y update
 # install general libraries like Java or ImageMagick
 yum -y install default-jre ImageMagick
 
-# add nodejs to yum
-#curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-#yum -y install nodejs #default-jre ImageMagick
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash    # for amazon linux
-. ~/.nvm/nvm.sh
-nvm install 6.9.1
-node -e "console.log('Running Node.js ' + process.version)"
+# # add nodejs to yum
+# #curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+# #yum -y install nodejs #default-jre ImageMagick
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash    # for amazon linux
+# . ~/.nvm/nvm.sh
+# nvm install 6.9.1
+# node -e "console.log('Running Node.js ' + process.version)"
 
-# ln -s ~/.nvm/versions/node/v6.9.1/bin/node /usr/local/bin/node
-# ln -s ~/.nvm/versions/node/v6.9.1/bin/node /usr/bin/node
-# ln -s ~/.nvm/versions/node/v6.9.1/bin/pm2 /usr/local/bin/pm2
-# ln -s ~/.nvm/versions/node/v6.9.1/bin/pm2 /usr/bin/pm2
-# ln -s ~/.nvm/versions/node/v6.9.1/bin/npm /usr/local/bin/pm2
-# ln -s ~/.nvm/versions/node/v6.9.1/bin/npm /usr/bin/pm2
+# # ln -s ~/.nvm/versions/node/v6.9.1/bin/node /usr/local/bin/node
+# # ln -s ~/.nvm/versions/node/v6.9.1/bin/node /usr/bin/node
+# # ln -s ~/.nvm/versions/node/v6.9.1/bin/pm2 /usr/local/bin/pm2
+# # ln -s ~/.nvm/versions/node/v6.9.1/bin/pm2 /usr/bin/pm2
+# # ln -s ~/.nvm/versions/node/v6.9.1/bin/npm /usr/local/bin/pm2
+# # ln -s ~/.nvm/versions/node/v6.9.1/bin/npm /usr/bin/pm2
 
-# install pm2 module globaly
-npm install -g pm2
-pm2 update
+# # install pm2 module globaly
+# npm install -g pm2
+# pm2 update
 
-cd /node
-npm install
+# cd ~/node
+# npm install
--- post_install.sh
+++ post_install.sh
@@ -2,18 +2,18 @@
 #!/usr/bin/env bash
 set -e
 
-# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash    # for amazon linux
-# . ~/.nvm/nvm.sh
-# nvm install 6.9.1
-# node -e "console.log('Running Node.js ' + process.version)"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash    # for amazon linux
+. ~/.nvm/nvm.sh
+nvm install 6.9.1
+node -e "console.log('Running Node.js ' + process.version)"
 
 
-# # install pm2 module globaly
-# npm install -g pm2
-# pm2 update
+# install pm2 module globaly
+npm install -g pm2
+pm2 update
 
-# cd ~/node
-# npm install
+cd ~/node
+npm install
 
 # setup NODE_ENV
 if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
