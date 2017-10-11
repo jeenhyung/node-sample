7820c9f05209489ff59c20e5bbb694b992352c1a
Jinhyung Park <jeenhyung@hanmail.net>
2017-10-11 17:50:39 +0900

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
-#!/usr/bin/env bash
-# if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
-#  export NODE_ENV=$DEPLOYMENT_GROUP_NAME
-# fi
 
 cd ~/node
 ./node_modules/pm2/bin/pm2 start src/app.js -n sample-node -i 0
