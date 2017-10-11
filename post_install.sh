ef6957ffd48c022d22bcf34413e18e853bbf463f
Jinhyung Park <jeenhyung@hanmail.net>
2017-10-11 19:46:49 +0900

    modify post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,21 +1,30 @@
 
 #!/usr/bin/env bash
-set -e
+# set -e
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash    # for amazon linux
-. ~/.nvm/nvm.sh
-nvm install 6.9.1
-node -e "console.log('Running Node.js ' + process.version)"
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash    # for amazon linux
+# . ~/.nvm/nvm.sh
+# nvm install 6.9.1
+# node -e "console.log('Running Node.js ' + process.version)"
 
-cd ~
+# cd ~/node
+# npm install --save pm2
+# # install pm2 module globaly
+# npm install -g pm2
+# pm2 update
+
+# cd ~/node
+# npm install
+
+wget http://nodejs.org/dist/v6.9.1/node-v6.9.1.tar.gz
+tar xzvf node-v4.4.7.tar.gz
+cd node-v6.9.1
+./configure
+make && make install
 
 cd ~/node
-npm install --save pm2
-# install pm2 module globaly
 npm install -g pm2
 pm2 update
-
-cd ~/node
 npm install
 
 # setup NODE_ENV
@@ -30,8 +39,8 @@ if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
     fi
 fi
 
-# # add node to startup
-# hasRc=`grep "su -l $USER" /etc/rc.d/rc.local | cat`
-# if [ -z "$hasRc" ]; then
-#     sudo sh -c "echo 'su -l $USER -c \"cd ~/node;sh ./run.sh\"' >> /etc/rc.d/rc.local"
-# fi
+# add node to startup
+hasRc=`grep "su -l $USER" /etc/rc.d/rc.local | cat`
+if [ -z "$hasRc" ]; then
+    sudo sh -c "echo 'su -l $USER -c \"cd ~/node;sh ./run.sh\"' >> /etc/rc.d/rc.local"
+fi
